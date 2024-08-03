bd328d377f88f4b51efd44a579df1ff2225dc015
VKalaitanov <kalaytanov93@gmail.com>
2024-08-03 17:31:20 +0300

    Удаление сенситивных данных

--- UTF_tz/settings.py
+++ UTF_tz/settings.py
@@ -1,20 +1,22 @@
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(env_file=Path('./.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@v4kvldi-g&^8aiifwmyb%(@pe4fas2anyi#y8h(q^zo9vfogg'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(env('DEBUG', default=1))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
 
 # Application definition
 
